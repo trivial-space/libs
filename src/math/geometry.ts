82fcb53a094225943d9e825b622d5248b550aace
trival <thomas.gorny@mail.com>
2016-10-09 14:12:45 +0200

    fix mirror matrix

--- src/math/geometry.ts
+++ src/math/geometry.ts
@@ -48,10 +48,10 @@ export function mirrorMatrixFromPlane(plane: GLVec): GLMat {
   let [a, b, c, d] = plane as number[]
 
   return mat4.fromValues(
-    1-2*a*a, -2*a*b, -2*a*c, -2*a*d,
-    -2*b*a, 1-2*b*b, -2*b*c, -2*b*d,
-    -2*c*a, -2*c*b, 1-2*c*c, -2*c*d,
-    0, 0, 0, 1
+    1-2*a*a, -2*a*b, -2*a*c, 0,
+    -2*a*b, 1-2*b*b, -2*b*c, 0,
+    -2*a*c, -2*b*c, 1-2*c*c, 0,
+    -2*a*d, -2*b*d, -2*c*d, 1
   )
 
 }
