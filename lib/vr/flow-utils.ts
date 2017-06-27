ee52294d4971180f0c9874ddaf6245f00206833e
trival <thomas.gorny@mail.com>
2017-06-27 13:58:29 +0200

    little painter utils update

--- lib/vr/flow-utils.ts
+++ lib/vr/flow-utils.ts
@@ -2,131 +2,130 @@ import { EntityRef, asyncStreamStart, stream, asyncStream } from 'tvs-flow/dist/
 import { WindowSizeState } from '../events/dom'
 import { create } from 'tvs-renderer/dist/lib/painter'
 import { getContext } from 'tvs-renderer/dist/lib/utils/context'
-import { Painter, Shade, Form, Sketch, Layer } from "tvs-renderer/dist/lib/render-types";
+import { Painter, Shade, Form, Sketch, Layer, GL } from 'tvs-renderer/dist/lib/render-types'
+import { unequal } from '../utils/predicates'
 
 
 export function makePainterCanvas (windowSizeEntity: EntityRef<WindowSizeState>) {
 
-  const canvas = asyncStreamStart<HTMLCanvasElement>(
-    null,
-    (send) => {
+	const canvas = asyncStreamStart<HTMLCanvasElement>(
+		null,
+		(send) => {
 
-      const canvas = document.createElement('canvas')
-      document.body.appendChild(canvas)
+			const canvas = document.createElement('canvas')
+			document.body.appendChild(canvas)
 
-      send(canvas)
+			send(canvas)
 
-      return () => {
-        document.body.removeChild(canvas)
-      }
-    }
-  )
+			return () => {
+				document.body.removeChild(canvas)
+			}
+		}
+	)
 
-  const gl = stream([canvas.HOT], getContext)
+	const gl = stream([canvas.HOT], getContext)
 
-  const painter: EntityRef<Painter> = asyncStream(
-    [gl.HOT],
-    (send, gl) => {
-      const p = create(gl)
-      send(p)
-      return p.destroy
-    }
-  )
+	const painter = asyncStream<Painter, GL>(
+		[gl.HOT],
+		(send, gl) => {
+			const p = create(gl)
+			send(p)
+			return p.destroy
+		}
+	)
+	.accept(unequal)
 
-  const canvasSize = stream(
-    [canvas.HOT, windowSizeEntity.HOT],
-    (canvas: HTMLCanvasElement) => ({
-      width: canvas.clientWidth,
-      height: canvas.clientHeight
-    })
-  )
+	const canvasSize = stream(
+		[canvas.HOT, windowSizeEntity.HOT],
+		(canvas: HTMLCanvasElement) => ({
+			width: canvas.clientWidth,
+			height: canvas.clientHeight
+		})
+	)
 
-  painter.react(
-    [canvasSize.HOT],
-    (p, _) => {
-      p.resize()
-      return p
-    },
-    'updateSize',
-  )
+	painter.react(
+		[canvasSize.HOT],
+		(p, _) => p.resize(),
+		'updateSize'
+	)
 
-  return { canvas, painter, gl, canvasSize }
+	return { canvas, painter, gl, canvasSize }
 }
 
 
 export function makeShadeEntity (painter: EntityRef<Painter>): EntityRef<Shade> {
-  return asyncStream(
-    [painter.HOT],
-    (send, painter) => {
-      const shade = painter.createShade()
-      send(shade)
-      return shade.destroy
-    }
-  )
+	return asyncStream(
+		[painter.HOT],
+		(send, painter) => {
+			const shade = painter.createShade()
+			send(shade)
+			return shade.destroy
+		}
+	)
 }
 
 export function makeFormEntity (painter: EntityRef<Painter>): EntityRef<Form> {
-  return asyncStream(
-    [painter.HOT],
-    (send, painter) => {
-      const form = painter.createForm()
-      send(form)
-      return form.destroy
-    }
-  )
+	return asyncStream(
+		[painter.HOT],
+		(send, painter) => {
+			const form = painter.createForm()
+			send(form)
+			return form.destroy
+		}
+	)
 }
 
 export function makeSketchEntity (painter: EntityRef<Painter>): EntityRef<Sketch> {
-  return asyncStream(
-    [painter.HOT],
-    (send, painter) => {
-      const sketch = painter.createSketch()
-      send(sketch)
-      return sketch.destroy
-    }
-  )
+	return asyncStream(
+		[painter.HOT],
+		(send, painter) => {
+			const sketch = painter.createSketch()
+			send(sketch)
+			return sketch.destroy
+		}
+	)
 }
 
 export function makeFlatSketchEntity (painter: EntityRef<Painter>): EntityRef<Sketch> {
-  return asyncStream(
-    [painter.HOT],
-    (send, painter) => {
-      const sketch = painter.createFlatSketch()
-      send(sketch)
-      return sketch.destroy
-    }
-  )
+	return asyncStream(
+		[painter.HOT],
+		(send, painter) => {
+			const sketch = painter.createFlatSketch()
+			send(sketch)
+			return sketch.destroy
+		}
+	)
 }
 
 export function makeStaticLayerEntity (painter: EntityRef<Painter>): EntityRef<Layer> {
-  return asyncStream(
-    [painter.HOT],
-    (send, painter) => {
-      const layer = painter.createStaticLayer()
-      send(layer)
-      return layer.destroy
-    }
-  )
+	return asyncStream(
+		[painter.HOT],
+		(send, painter) => {
+			const layer = painter.createStaticLayer()
+			send(layer)
+			return layer.destroy
+		}
+	)
 }
 
 export function makeDrawingLayerEntity (painter: EntityRef<Painter>): EntityRef<Layer> {
-  return asyncStream(
-    [painter.HOT],
-    (send, painter) => {
-      const layer = painter.createDrawingLayer()
-      send(layer)
-      return layer.destroy
-    }
-  )
+	return asyncStream(
+		[painter.HOT],
+		(send, painter) => {
+			const layer = painter.createDrawingLayer()
+			send(layer)
+			return layer.destroy
+		}
+	)
 }
 
 export function makeEffectLayerEntity (painter: EntityRef<Painter>): EntityRef<Layer> {
-  return asyncStream(
-    [painter.HOT],
-    (send, painter) => {
-      const layer = painter.createEffectLayer()
-      send(layer)
-      return layer.destroy
-    }
-  )
+	return asyncStream(
+		[painter.HOT],
+		(send, painter) => {
+			const layer = painter.createEffectLayer()
+			send(layer)
+			return layer.destroy
+		}
+	)
 }
