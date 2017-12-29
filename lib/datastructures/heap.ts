4b06f606c4a2e8073525637168b1e5498727e938
trival <thomas.gorny@mail.com>
2017-12-29 17:47:08 +0100

    implement heap datastructure

---
+++ lib/datastructures/heap.ts
@@ -0,0 +1,106 @@
+export interface Heap<T> {
+	insert: (item: T) => void
+	getTop: () => T
+	pull: () => T
+	size: () => number
+	fromArray: (items: T[]) => void
+}
+
+
+export type HeapCompareFn<T> = (parent: T, child: T) => boolean
+
+
+export function parentIndex (currentIndex: number) {
+	return Math.floor(currentIndex / 2)
+}
+
+export function rightIndex (currentIndex: number) {
+	return currentIndex * 2 + 1
+}
+
+export function leftIndex (currentIndex: number) {
+	return currentIndex * 2
+}
+
+
+function swap(i: number, j: number, arr: any[]) {
+	const temp = arr[i]
+	arr[i] = arr[j]
+	arr[j] = temp
+}
+
+
+export function heapifyAt<T> (compareFn: HeapCompareFn<T>, arr: T[], i: number) {
+	const l = leftIndex(i)
+	const r = rightIndex(i)
+	let top = i
+	if (l < arr.length && !compareFn(arr[top], arr[l])) {
+		top = l
+	}
+	if (r < arr.length && !compareFn(arr[top], arr[r])) {
+		top = r
+	}
+	if (top !== i) {
+		swap(i, top, arr)
+		heapifyAt(compareFn, arr, top)
+	}
+}
+
+
+export function heapify<T> (compareFn: HeapCompareFn<T>, arr: T[]) {
+	for (let i = Math.floor((arr.length - 1) / 2); i >= 0; i--) {
+		heapifyAt(compareFn, arr, i)
+	}
+}
+
+
+export function insert<T> (compareFn: HeapCompareFn<T>, arr: T[], item: T) {
+	arr.push(item)
+	if (arr.length > 1) {
+		let i = arr.length - 1
+		let p = parentIndex(i)
+		while (i > 0 && !compareFn(arr[p], arr[i])) {
+			swap(p, i, arr)
+			i = p
+			p = parentIndex(i)
+		}
+	}
+}
+
+
+export function createHeap<T> (compareFn: HeapCompareFn<T>) {
+	let arr: T[] = []
+
+	function size () {
+		return arr.length
+	}
+
+	function fromArray(a: T[]) {
+		arr = a.concat()
+		heapify(compareFn, arr)
+	}
+
+	function pull() {
+		const top = arr.shift()
+		heapifyAt(compareFn, arr, 0)
+		return top
+	}
+
+	function getTop() {
+		return arr[0]
+	}
+
+	return {
+		size,
+		fromArray,
+		pull,
+		getTop,
+		insert: (item: T) => insert(compareFn, arr, item)
+	} as Heap<T>
+}
+
+
+export function maxHeapCompareFn (parent: number, child: number) {
+	return parent >= child
+}
+
---
+++ spec/datastructures/heap-test.ts
@@ -0,0 +1,114 @@
+import { createHeap, maxHeapCompareFn, Heap } from 'datastructures/heap'
+import { expect } from 'chai'
+import { N, S } from '../types'
+
+
+describe('datastructures heap', function() {
+
+	describe('maxHeap', function() {
+		let heap: Heap<N>
+
+		beforeEach(function() {
+			heap = createHeap(maxHeapCompareFn)
+		})
+
+		it('is initially empty', function() {
+			expect(heap.size()).to.equal(0)
+		})
+
+		it('can be created from an array', function() {
+			heap.fromArray([1, 2, 3])
+			expect(heap.size()).to.equal(3)
+		})
+
+		it('can be inspect the highest value', function() {
+			heap.fromArray([1, 3, 2])
+			expect(heap.getTop()).to.equal(3)
+			expect(heap.size()).to.equal(3)
+		})
+
+		it('returns the biggest item by pulling', function() {
+			heap.fromArray([2, 4, 3, 1])
+			expect(heap.size()).to.equal(4)
+
+			expect(heap.pull()).to.equal(4)
+			expect(heap.size()).to.equal(3)
+
+			expect(heap.pull()).to.equal(3)
+			expect(heap.size()).to.equal(2)
+
+			expect(heap.pull()).to.equal(2)
+			expect(heap.size()).to.equal(1)
+
+			expect(heap.pull()).to.equal(1)
+			expect(heap.size()).to.equal(0)
+		})
+
+		it('can insert new elements', function() {
+			heap.fromArray([3, 6])
+			heap.insert(5)
+			heap.insert(10)
+
+			expect(heap.pull()).to.equal(10)
+			expect(heap.pull()).to.equal(6)
+			expect(heap.pull()).to.equal(5)
+			expect(heap.pull()).to.equal(3)
+		})
+
+
+		it('returns undefined on pulling on empty heap', function() {
+			expect(heap.pull()).to.be.undefined
+			expect(heap.pull()).to.be.undefined
+			expect(heap.pull()).to.be.undefined
+
+			expect(heap.size()).to.equal(0)
+		})
+	})
+
+
+	it('can build heaps with custom compare functions', function() {
+		const minHeap = createHeap((p: N, c: N) => p <= c)
+		minHeap.fromArray([3, 6])
+		minHeap.insert(5)
+		minHeap.insert(10)
+
+		expect(minHeap.size()).to.equal(4)
+
+		expect(minHeap.pull()).to.equal(3)
+		expect(minHeap.size()).to.equal(3)
+
+		expect(minHeap.pull()).to.equal(5)
+		expect(minHeap.size()).to.equal(2)
+
+		expect(minHeap.pull()).to.equal(6)
+		expect(minHeap.size()).to.equal(1)
+
+		expect(minHeap.pull()).to.equal(10)
+		expect(minHeap.size()).to.equal(0)
+
+
+		const stringHeap = createHeap((p: S, c: S) => p.localeCompare(c) >= 0)
+		stringHeap.fromArray(['z', 'f'])
+		stringHeap.insert('a')
+		stringHeap.insert('w')
+		stringHeap.insert('z')
+
+		expect(stringHeap.size()).to.equal(5)
+
+		expect(stringHeap.pull()).to.equal('z')
+		expect(stringHeap.size()).to.equal(4)
+
+		expect(stringHeap.pull()).to.equal('z')
+		expect(stringHeap.size()).to.equal(3)
+
+		expect(stringHeap.pull()).to.equal('w')
+		expect(stringHeap.size()).to.equal(2)
+
+		expect(stringHeap.pull()).to.equal('f')
+		expect(stringHeap.size()).to.equal(1)
+
+		expect(stringHeap.pull()).to.equal('a')
+		expect(stringHeap.size()).to.equal(0)
+	})
+})
+
