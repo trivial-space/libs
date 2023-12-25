0e18e6358c0ad838607736f6d283ac335e176575
Thomas Gorny <thomas.gorny@pm.me>
2023-12-25 10:24:18 +0100

    publish

--- jest.config.js
+++ jest.config.js
@@ -3,4 +3,13 @@ export default {
 	testEnvironment: 'node',
 	moduleDirectories: ['node_modules', 'src'],
 	testPathIgnorePatterns: ['<rootDir>/dist/', '<rootDir>/node_modules/'],
+	extensionsToTreatAsEsm: ['.ts'],
+	moduleNameMapper: {
+		'^(\\.{1,2}/.*)\\.js$': '$1',
+	},
+	globals: {
+		'ts-jest': {
+			useESM: true,
+		},
+	},
 }
